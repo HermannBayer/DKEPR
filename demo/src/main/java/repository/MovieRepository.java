ba329ed41fde544096cf4bc863fc69e2304f645a
HermannBayer <hermann.bayer@aon.at>
2021-03-18 16:16:49 +0100

    RegService

--- demo/src/main/java/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import org.neo4j.driver.internal.shaded.reactor.core.publisher.Mono;
-import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
-
-public interface MovieRepository extends ReactiveNeo4jRepository<MovieEntity, String> {
-	Mono<MovieEntity> findOneByTitle(String title);
-}
--- demo2/src/main/java/com/example/demo2/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo2;
-
-import java.util.List;
-
-import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
-
-public interface MovieRepository extends ReactiveNeo4jRepository<MovieEntity, String> {
-	List<MovieEntity> findOneByTitle(String title);
-}
